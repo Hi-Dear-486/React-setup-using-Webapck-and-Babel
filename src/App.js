8e8a1893b44a26d1145c3b392829729bd1416d6a
Muhammad zeeshan <fullstackdeveloper486@gmail.com>
2024-12-07 22:58:45 +0500

    setup webpack & babel

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Create React Project using webpack and babel</h1>
+    </div>
+  );
+};
+
+export default App;
+2;
